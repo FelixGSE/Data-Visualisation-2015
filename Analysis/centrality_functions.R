6635da6f00f2c4ea7e55f58132a452298a49b12f
FelixGSE <felix@Felixs-MacBook-Pro.local>
2016-04-10 23:55:54 +0200

    Push project

---
+++ Analysis/centrality_functions.R
@@ -0,0 +1,128 @@
+################################################################################
+
+################################################################################
+
+# Author:       Felix Gutmann
+# Programm:     Barcelona graduate school of economics - M.S. Data Science 
+# Course:       14D007 Data Visualization
+# Last update:  09.04.2015
+
+# Decription:   This file initializes both centrality meassures for Input-
+#               Output Tables  
+
+################################################################################
+# Random closeness centrality
+################################################################################
+
+random.walk.closeness.centrality <- function( A ){
+  
+  N <- nrow( A )
+  H <- matrix( 0, nrow = N, ncol = N )
+  A <- diag( N ) - solve( diag( colSums( t(A) ) ) ) %*% A
+  I <- solve( A[2:N,2:N] )
+  
+  for( i in 1:N ){
+    
+    
+    ifelse( ( i - 1 ) == 0 ,
+            index01 <- c( (i+1):N ),
+            ifelse( i == N, 
+                    index01 <- c( 1:(i-1)  ),
+                    index01 <- c( 1:(i-1) , (i+1):N )
+            )
+    )
+    
+    ifelse( (i+2) > N, 
+            index02 <- c( 1:i  ),
+            index02 <- c( 1:i , (i+2):N )
+    )
+    
+    
+    
+    H[index01,i] <- I %*% rep(1, ( N - 1 ) )
+    
+    if( i < N ){
+      
+      u   <- A[ index02, i  ] - A[ index01, (i+1) ]
+      n01 <- as.numeric( ( 1 + I[ i , ] %*% u ) )
+      I   <- I - ( ( I %*% u ) %*% I[ i , ]  ) / n01
+      v   <- A[ i , index02 ] - A[ (i+1), index01 ]
+      n02 <-  as.numeric(  ( 1+ v %*% I[,i] ) ) 
+      I   <- I - ( I[ ,i] %*% ( v %*% I ) ) / n02
+      I   <- solve( A[index02, index02 ]  )
+      
+      if( any(is.infinite( I ) ) ){
+        
+        I <- solve( A[ index02, index02 ] )
+        
+      } 
+    }
+  }
+  
+  final.result <- N / colSums(H) 
+  
+  return( final.result )
+  
+}
+
+################################################################################
+# Random Walk counting centrality
+################################################################################
+
+random.walk.counting.centrality <- function( A ){
+  
+  # Compute Dimension - Nrow is enough since A is quadratic
+  N               <- nrow( A )
+  full.dimension  <- 1:N
+  
+  # Compute D - Matrix
+  D    <- diag( rowSums( A , na.rm = FALSE ) ) 
+  
+  # Compute take matrix
+  temp <- ifelse( A > 0 , 1 , 0 )
+  temp <- ifelse( ( temp + t( temp ) ) > 0 , 1, 0 )
+  Take <- temp * 0.5
+
+  # Initialize result matrix - This is very inefficient!!!
+  result <- matrix( NA , nrow = 0 , ncol = N ) 
+  
+  # Compute centrality - This is where the magic happens
+  for( t in 1:N ){
+    
+    # Compute transition matrix for node t - exclude t from network
+    temp.dimension <- setdiff( full.dimension , t )
+    A.temp         <- A[temp.dimension,temp.dimension]
+    D.temp         <- D[temp.dimension,temp.dimension]
+    T.temp         <- solve( D.temp - A.temp )
+    
+    # Subset take matrix for node t
+    Take.temp      <- Take[temp.dimension,temp.dimension]
+    
+    # Compute centrality for node t
+    for( s in 1:( N - 1 ) ){
+      
+      Z.temp  <- diag( as.numeric( T.temp[s,] %*% A.temp ) ) 
+      W.temp  <- ( Z.temp + t( Z.temp ) ) * Take.temp
+    
+      cs.temp <- as.numeric( colSums( W.temp , na.rm = FALSE ) )
+      rs.temp <- as.numeric( rowSums( W.temp , na.rm = FALSE ) )
+      
+      cm.temp <- append( 0.5 * ( cs.temp + rs.temp ) , 0 , (t - 1) )
+      result  <- rbind( result , cm.temp )  
+   
+    }
+  }
+  
+  # Design final centrality meassure
+  cm.final     <- colSums(result) +  2 * ( N - 1 ) * rep(1, N )
+  numerator    <- ( N * ( N - 1 ) ) 
+  final.result <- cm.final / numerator 
+  
+  # Return centrality
+  return( final.result )
+  
+}
+
+################################################################################
+
+################################################################################
---
+++ Analysis/edgelist.R
@@ -0,0 +1,157 @@
+################################################################################
+
+################################################################################
+
+# Author:       Felix Gutmann
+# Programm:     Barcelona graduate school of economics - M.S. Data Science 
+# Course:       14D007 Data Visualization
+# Last update:  11.04.2016
+
+# Decription:   This file computes 
+
+################################################################################
+### Preamble
+################################################################################
+
+### Clear workspace
+rm(list = ls())
+
+### Load data
+setwd('/Users/felix/Documents/GSE/Term 2/14D007 Data Visualization/Project/Literature and data/Raw_data/')
+iot          <- read.csv( 'iot_clean.csv', sep = ';', header = FALSE )
+sector       <- read.csv( 'product_groups_clean.csv', sep = ';', header = FALSE )
+value.added  <- read.csv( 'value_added_clean.csv', sep = ';', header = FALSE )
+employment   <- read.csv( 'labour_input_clean.csv',sep = ';',header=FALSE )
+final.demand <- read.csv( 'final_demand_clean.csv',sep = ';',header=FALSE )
+### Load Packages 
+
+### Set options
+options( useFancyQuotes = FALSE )
+
+### Initialize auxilliary functions
+setwd('/Users/felix/Documents/GSE/Term 2/14D007 Data Visualization/Project/Analysis/')
+source('centrality_functions.R')
+
+################################################################################
+# Compute centrality meassures for IO-Graph
+################################################################################
+
+# Convert data frame to class matrix and delete column names
+N   <- nrow( iot )
+
+# Compute binary adjacency matrix and both in and outlist
+adjacency.matrix <- ifelse( iot > 0 , 1 , 0 )
+in.list          <- colSums(adjacency.matrix)
+out.list         <- rowSums(adjacency.matrix)
+
+# Find unconnected and absorbing nodes 
+zero      <- which( out.list == 0 & in.list == 0 )
+absorbing <- which( out.list  < 2 & in.list  > 0 )
+drops     <- c( zero, absorbing )
+
+# Clean IOT - Drop found sectors
+all.sectors <- 1:N
+keeps       <- setdiff( all.sectors , drops )
+iot.clean   <- as.matrix( iot[ keeps , keeps ] )
+
+# Compute centrality meassures
+rwclose <- random.walk.closeness.centrality(iot.clean)
+rwcount <- random.walk.counting.centrality(iot.clean)
+
+# Adjust output - Assign zero for 
+rw.closeness <- c( )
+rw.counting  <- c( ) 
+drop.counter <- drops
+i <- 1
+j <- 1
+while ( i < ( N + 1)  ) {
+  
+  bool <- i == drop.counter
+  
+  if( any( bool ) ){
+    pos <- which( bool == i )
+    drop.counter <- setdiff( drop.counter, drop.counter[pos] )
+    rw.closeness[i] <- 0
+    rw.counting[i]  <- 0
+  } else {
+    rw.closeness[i] <- rwclose[j]
+    rw.counting[i]  <- rwcount[j]
+    j <- j + 1
+  }
+    i <- i+1
+}
+
+################################################################################
+# Covert data to d3 format
+################################################################################
+
+# Initialize empty node list
+node.list <- c( )
+
+# Compute and format nodes
+for( i in 1:N ){
+  
+node.list[i] <- ifelse( i < N, 
+  paste0('{ name:',sQuote(sector[i,1]),',nor:',10,
+         ',clo:',rw.closeness[i] * 1000 ,',cou:',rw.counting[i],
+         ',em:',employment[1,i],',va:',value.added[1,i] ,',fd:', final.demand[1,i] , '},'),
+  paste0('{ name:',sQuote(sector[i,1]),',nor:',10,
+         ',clo:',rw.closeness[i] * 1000,',cou:',rw.counting[i],
+         ',em:',employment[1,i],',va:',value.added[1,i] ,',fd:', final.demand[1,i] ,'}')
+  )
+  
+}
+
+# Remove quotes
+node.list <- noquote(node.list)
+
+# Initialize empty edge list
+edge.list <- c(NA)
+
+# Compute edge list for java script
+for( i in 1:N){
+    
+  # Compute out list for node i
+  temp.out.list <- adjacency.matrix[i,]
+  
+    # Loop over adjacent nodes j of node i
+    for( j in 1:N ){
+      
+      # If there is an edge append to list
+      if(  temp.out.list[j] != 0 ){
+        
+        edge <- paste0('{ source:',(i-1),', target:',(j-1),'},')
+        current.index <- length(edge.list) + 1 
+        edge.list[current.index] <- edge
+        
+        }
+      
+    }
+  
+  # Last iteration add no comma 
+  if( i == N ){ 
+    last.index <- current.index - 1
+    edge.list[last.index] <- paste0('{ source:',(i-1),', target:',(j-1),'}') 
+    edge.list <- edge.list[2:last.index]
+    }
+}
+
+# Design final output
+open   <- "var dataset = { nodes: ["
+bridge <-  "],edges: ["
+close  <- "]};"
+graph  <- noquote( c( open , node.list , bridge , edge.list , close ) )
+
+################################################################################
+
+################################################################################
+
+# Reset working directory for output
+setwd( '/Users/felix/Documents/GSE/Term 2/14D007 Data Visualization/Project/App/data/' )
+
+# Save data to HD
+write( graph , 'graph_data.js' )
+
+################################################################################
+
+################################################################################
